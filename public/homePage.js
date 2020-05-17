9bb549c43612c33254d772f620db3820b2136aa6
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2020-05-17 14:10:04 +0300

    Update homePage.js

--- public/homePage.js
+++ public/homePage.js
@@ -1,85 +1,86 @@
 'use strict';
 
- const logoutBtn = new LogoutButton();
+const logoutBtn = new LogoutButton();
 
- logoutBtn.action = f => ApiConnector.logout(function(e){
-    if(e.success === true) {
+logoutBtn.action = f => ApiConnector.logout(function(response){
+    if(response.success) {
         location.reload();
     }
 });
 
- ApiConnector.current(function(e){
-    if (e.success === true) {
-        ProfileWidget.showProfile(e.data);
+ApiConnector.current(function(response){
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
     }
 });
 
- let RB = new RatesBoard();
+let ratesBoard = new RatesBoard();
 
- setInterval(function(){ApiConnector.getStocks(function(e){
-    if (e.success === true) {
-        RB.clearTable();
-        RB.fillTable(e.data);
+setInterval(function(){ApiConnector.getStocks(function(response){
+    if (response.success) {
+        ratesBoard.clearTable();
+        ratesBoard.fillTable(response.data);
     }
 });}(), 60000);
 
- let MM = new MoneyManager();
+let moneyManager = new MoneyManager();
 
- MM.addMoneyCallback = data => ApiConnector.addMoney(data, function(e){
-    if (e.success === false) {
-        MM.setMessage(true, "Ошибка");
+moneyManager.addMoneyCallback = data => ApiConnector.addMoney(data, function(response){
+    if (!response.success) {
+        moneyManager.setMessage(true, response.data);
     } else {
-        ProfileWidget.showProfile(e.data);
-        MM.setMessage(false, "Успешно");
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(false, "РЈСЃРїРµС€РЅРѕРµ РґРѕР±Р°РІР»РµРЅРёРµ СЃСЂРµРґСЃС‚РІ");
     }
 });
 
- MM.conversionMoneyCallback = data => ApiConnector.convertMoney(data, function(e){
-    if (e.success === false) {
-        MM.setMessage(true, "Ошибка");
+moneyManager.conversionMoneyCallback = data => ApiConnector.convertMoney(data, function(response){
+    if (!response.success) {
+        moneyManager.setMessage(true, response.data);
     } else {
-        ProfileWidget.showProfile(e.data);
-        MM.setMessage(false, "Успешно");
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(false, "РЈСЃРїРµС€РЅС‹Р№ РїРµСЂРµРІРѕРґ СЃСЂРµРґСЃС‚РІ");
     }
 });
 
- MM.sendMoneyCallback = data => ApiConnector.transferMoney(data, function(e){
-    if (e.success === false) {
-        MM.setMessage(true, "Ошибка");
+moneyManager.sendMoneyCallback = data => ApiConnector.transferMoney(data, function(response){
+    if (!response.success) {
+        console.log(response);
+        moneyManager.setMessage(true, response.data);
     } else {
-        ProfileWidget.showProfile(e.data);
-        MM.setMessage(false, "Успешно");
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(false, "РЈСЃРїРµС€РЅР°СЏ РѕС‚РїСЂР°РІРєР° СЃСЂРµРґСЃС‚РІ");
     }
 });
 
- let FW = new FavoritesWidget();
+let favoritesWidget = new FavoritesWidget();
 
- ApiConnector.getFavorites(function(e){
-    if (e.success === true) {
-        FW.clearTable();
-        FW.fillTable(e.data);
-        MM.updateUsersList(e.data);
+ApiConnector.getFavorites(function(response){
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
     }
 }); 
 
- FW.addUserCallback = f => ApiConnector.addUserToFavorites(f,function(e){
-    if (e.success === true) {
-        FW.clearTable();
-        FW.fillTable(e.data);
-        MM.updateUsersList(e.data);
-        FW.setMessage(false, "Успешно");
+favoritesWidget.addUserCallback = f => ApiConnector.addUserToFavorites(f,function(response){
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        favoritesWidget.setMessage(false, "РЈСЃРїРµС€РЅРѕРµ РґРѕР±Р°РІР»РµРЅРёРµ РІ РёР·Р±СЂР°РЅРЅС‹Рµ");
     } else {
-        FW.setMessage(true, "Ошибка");
+        favoritesWidget.setMessage(true, response.data);
     }
 });
 
- FW.removeUserCallback = f => ApiConnector.removeUserFromFavorites(f,function(e){
-    if (e.success === true) {
-        FW.clearTable();
-        FW.fillTable(e.data);
-        MM.updateUsersList(e.data);
-        FW.setMessage(false, "Успешно");
+favoritesWidget.removeUserCallback = f => ApiConnector.removeUserFromFavorites(f,function(response){
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        favoritesWidget.setMessage(false, "РЈСЃРїРµС€РЅРѕРµ СѓРґР°Р»РµРЅРёРµ РёР· РёР·Р±СЂР°РЅРЅС‹Рµ");
     } else {
-        FW.setMessage(true, "Ошибка");
+        favoritesWidget.setMessage(true, response.data);
     }
 });
