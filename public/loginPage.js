4a986d8747386e7807706cb0e63df7bf494ac008
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2020-05-17 14:08:36 +0300

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,16 +1,17 @@
 'use strict';
+
 const userForm = new UserForm();
-userForm.loginFormCallback = data => ApiConnector.login(data, function(e) {
-    if (e.success === true) {
+userForm.loginFormCallback = data => ApiConnector.login(data, function(response) {
+    if (response.success) {
         location.reload();
     } else {
-        userForm.setLoginErrorMessage("Ошибка входа");
+        userForm.setLoginErrorMessage(response.data);
     }
 });
-userForm.registerFormCallback = data => ApiConnector.register(data, function(e) {
-    if (e.success === true) {
+userForm.registerFormCallback = data => ApiConnector.register(data, function(response) {
+    if (response.success) {
         location.reload();
     } else {
-        userForm.setRegisterErrorMessage("Пользователь уже существует");
+        userForm.setRegisterErrorMessage(response.data);
     }
 });
